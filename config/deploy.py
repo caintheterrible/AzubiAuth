90b22fff597e01c4b7edcc150d8c046d61aeab33
cain. <caintheterrible@gmail.com>
2025-05-30 21:51:18 +0000

    ADDED: default  configurations

---
+++ config/deploy.py
@@ -0,0 +1,6 @@
+from config.base import configurations
+# from config.database import *
+# import config.env
+
+ALLOWED_HOSTS:list=configurations.allowed_hosts
+ROOT_URLCONF:str=configurations.root_url_configurations
