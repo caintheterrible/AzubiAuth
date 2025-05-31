d399b1d27be1cf8f23e6de8e0290a9d61c2097a3
cain. <caintheterrible@gmail.com>
2025-05-31 05:14:54 +0000

    updated module calls

--- config/deploy.py
+++ config/deploy.py
@@ -2,5 +2,5 @@ from config.base import base_configurations
 from config.database import db_configurations
 # import config.env
 
-ALLOWED_HOSTS:list=configurations.allowed_hosts
-ROOT_URLCONF:str=configurations.root_url_configurations
+ALLOWED_HOSTS:list=base_configurations.allowed_hosts
+ROOT_URLCONF:str=base_configurations.root_url_configurations
