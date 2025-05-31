c9efdd0e30bea7cdc4828b73f5671a7261466a25
cain. <caintheterrible@gmail.com>
2025-05-31 05:58:28 +0000

    UPDATED: added temporary DATABASES configurations for testing purposes

--- config/deploy.py
+++ config/deploy.py
@@ -1,6 +1,15 @@
+from pathlib import Path
 from config.base import base_configurations
 from config.database import db_configurations
 # import config.env
 
+BASE_DIR:Path=base_configurations.base_dir
 ALLOWED_HOSTS:list=base_configurations.allowed_hosts
 ROOT_URLCONF:str=base_configurations.root_url_configurations
+
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
+    }
+}
